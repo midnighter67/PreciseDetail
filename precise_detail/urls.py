e8ad438db12fec50aeeed6e9e8b0c3f463ad8990
Mike Wooldridge <midnighter67@gmail.com>
2024-08-30 04:20:30 -0600

    setting up navbar

--- precise_detail/urls.py
+++ precise_detail/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+     path('quote', views.quote, name='quote')
 ]
--- precise_detail/views.py
+++ precise_detail/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     """ Initial landing page  """
     return render(request, 'home.html', {})
+
+def quote(request):
+    """ Form for quote request  """
+    return render(request, 'quote.html', {})
