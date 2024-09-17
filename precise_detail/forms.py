dc0d9610f13c449c1249b7c5a347e77409c410f6
Mike Wooldridge <midnighter67@gmail.com>
2024-09-17 15:27:25 -0600

    added media query settings

--- precise_detail/forms.py
+++ precise_detail/forms.py
@@ -49,20 +49,7 @@ class EstimateForm(forms.Form):
             raise forms.ValidationError('this field is required')
         return self.cleaned_data
     """
-    """
-    name = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    email = forms.EmailField(widget= forms.Textarea(attrs={"class": "form-control", "rows": "5"}))
-    phone = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    address = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    city = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    zip = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    bed = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    bath = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    sqft = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    pets = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    frequency = forms.CharField(widget= forms.TexTextInputarea(attrs={"class": "form-control", "rows": "5"}))
-    comment = forms.CharField(widget= forms.TextInput(attrs={"class": "form-control", "rows": "5"}))
-    """
+    
         
         
     
